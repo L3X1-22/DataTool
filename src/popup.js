de1390af86bffefd3d1bcc7c87e9dc9e04e56f1c
Alexia Gutierrez Niño <alexia300622@gmail.com>
2026-05-11 19:28:35 -0500

    created fetch functions

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,8 @@
+import * as dfd from "https://cdn.jsdelivr.net/npm/danfojs@1.2.0/dist/danfojs.esm.min.js";
+
+/*
+  Dark/light theme handler
+*/
 const toggle = document.getElementById("themeToggle");
 
 toggle.addEventListener("change", () => {
@@ -7,3 +12,82 @@ toggle.addEventListener("change", () => {
     document.documentElement.removeAttribute("data-theme");
   }
 });
+
+/* 
+  Functions to fetch, normalize and return data
+*/
+
+function process(){
+  let dates = [new Date(document.getElementById("startDate").value), endDate = new Date(document.getElementById("endDate").value)];
+
+  dates = SortDates(dates);
+
+  console.log(dates[0].toISOString().split('T')[0],dates[1].toISOString().split('T')[0]);
+}
+
+function SortDates(dates){
+    if(dates[0] > dates[1]) {
+    let temp = dates[0];
+    dates[0] = dates[1];
+    dates[1] = temp;
+    return dates;
+  } return dates;
+}
+
+async function fetchCSV(date){
+  const res = await fetch("https://integrify.claro.net.co:8443/_dash-update-component", {
+  method: "POST",
+  headers: {
+    "accept": "application/json",
+    "content-type": "application/json"
+  },
+  credentials: "include",
+  body: JSON.stringify({
+    output: "descargar_archivo.data",
+    outputs: {
+      id: "descargar_archivo",
+      property: "data"
+    },
+    inputs: [
+      {
+        id: "dowload_equipos",
+        property: "n_clicks",
+        value: 2
+      }
+    ],
+    changedPropIds: ["dowload_equipos.n_clicks"],
+    parsedChangedPropsIds: ["dowload_equipos.n_clicks"],
+    state: [
+      {
+        id: "textinput_fecha",
+        property: "value",
+        value: date
+      },
+      {id:"input_hostname",property:"value",value:null},
+      {id:"input_serial",property:"value",value:null},
+      {id:"input_ip_cpe",property:"value",value:null},
+      {id:"input_modelo",property:"value",value:null},
+      {id:"input_fabricante",property:"value",value:null},
+      {id:"input_pe",property:"value",value:null},
+      {id:"input_version",property:"value",value:null},
+      {id:"input_version_soft",property:"value",value:null},
+      {id:"input_vrf_pe",property:"value",value:null}
+    ]
+  })
+})
+
+  const data = await res.json();
+  const fileData = data.response.descargar_archivo.data;
+
+  const blob = new Blob([fileData.content], { type: "text/csv" });
+
+  return blob;
+}
+
+function fetchDateRange(dates){
+  let dataframes;
+  if(dates[0]==dates[1]){
+    dataframes = new fetchCSV(dates[0]);
+
+  }
+}
