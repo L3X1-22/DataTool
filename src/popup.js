92a8a57808580e2c65a18f3cc4ecd9149fbf4059
Alexia Gutierrez Niño <alexia300622@gmail.com>
2026-05-13 00:44:15 -0500

    Created function to create the dataframe to work with

--- src/popup.js
+++ src/popup.js
@@ -17,16 +17,16 @@ toggle.addEventListener("change", () => {
   Functions to fetch, normalize and return data
 */
 
-function process(){
+function process() {
   let dates = [new Date(document.getElementById("startDate").value), endDate = new Date(document.getElementById("endDate").value)];
 
   dates = SortDates(dates);
 
-  console.log(dates[0].toISOString().split('T')[0],dates[1].toISOString().split('T')[0]);
+  console.log(dates[0].toISOString().split('T')[0], dates[1].toISOString().split('T')[0]);
 }
 
-function SortDates(dates){
-    if(dates[0] > dates[1]) {
+function SortDates(dates) {
+  if (dates[0] > dates[1]) {
     let temp = dates[0];
     dates[0] = dates[1];
     dates[1] = temp;
@@ -34,47 +34,47 @@ function SortDates(dates){
   } return dates;
 }
 
-async function fetchCSV(date){
+async function fetchCSV(date) {
   const res = await fetch("https://integrify.claro.net.co:8443/_dash-update-component", {
-  method: "POST",
-  headers: {
-    "accept": "application/json",
-    "content-type": "application/json"
-  },
-  credentials: "include",
-  body: JSON.stringify({
-    output: "descargar_archivo.data",
-    outputs: {
-      id: "descargar_archivo",
-      property: "data"
+    method: "POST",
+    headers: {
+      "accept": "application/json",
+      "content-type": "application/json"
     },
-    inputs: [
-      {
-        id: "dowload_equipos",
-        property: "n_clicks",
-        value: 2
-      }
-    ],
-    changedPropIds: ["dowload_equipos.n_clicks"],
-    parsedChangedPropsIds: ["dowload_equipos.n_clicks"],
-    state: [
-      {
-        id: "textinput_fecha",
-        property: "value",
-        value: date
+    credentials: "include",
+    body: JSON.stringify({
+      output: "descargar_archivo.data",
+      outputs: {
+        id: "descargar_archivo",
+        property: "data"
       },
-      {id:"input_hostname",property:"value",value:null},
-      {id:"input_serial",property:"value",value:null},
-      {id:"input_ip_cpe",property:"value",value:null},
-      {id:"input_modelo",property:"value",value:null},
-      {id:"input_fabricante",property:"value",value:null},
-      {id:"input_pe",property:"value",value:null},
-      {id:"input_version",property:"value",value:null},
-      {id:"input_version_soft",property:"value",value:null},
-      {id:"input_vrf_pe",property:"value",value:null}
-    ]
+      inputs: [
+        {
+          id: "dowload_equipos",
+          property: "n_clicks",
+          value: 2
+        }
+      ],
+      changedPropIds: ["dowload_equipos.n_clicks"],
+      parsedChangedPropsIds: ["dowload_equipos.n_clicks"],
+      state: [
+        {
+          id: "textinput_fecha",
+          property: "value",
+          value: date
+        },
+        { id: "input_hostname", property: "value", value: null },
+        { id: "input_serial", property: "value", value: null },
+        { id: "input_ip_cpe", property: "value", value: null },
+        { id: "input_modelo", property: "value", value: null },
+        { id: "input_fabricante", property: "value", value: null },
+        { id: "input_pe", property: "value", value: null },
+        { id: "input_version", property: "value", value: null },
+        { id: "input_version_soft", property: "value", value: null },
+        { id: "input_vrf_pe", property: "value", value: null }
+      ]
+    })
   })
-})
 
   const data = await res.json();
   const fileData = data.response.descargar_archivo.data;
@@ -84,10 +84,37 @@ async function fetchCSV(date){
   return blob;
 }
 
-function fetchDateRange(dates){
-  let dataframes;
-  if(dates[0]==dates[1]){
-    dataframes = new fetchCSV(dates[0]);
+async function fetchDateRange(dates) {
+  const dataframes = [];
+  if (dates[0].getTime() == dates[1].getTime()) {
+    dataframes[0] = await fetchCSV(dates[0].toISOString().split('T')[0]);
+  } else {
+    let j = 0;
 
+    for (let i = new Date(dates[0].getTime()); i <= dates[1]; i.setDate(i.getDate() + 1)) {
+      dataframes[j] = await fetchCSV(i.toISOString().split('T')[0]);
+      j++;
+    }
   }
+  return dataframes;
+}
+
+async function dataframesCreator(blobs) {
+  let dataframe = new dfd.DataFrame([]);
+
+  for (let i = 0; i < blobs.length; i++) {
+    const text = await blobs[i].text();
+    const df = await dfd.readCSV(text);
+
+    if (i == 0) {
+      dataframe = df;
+    } else {
+      dataframe = dfd.concat({
+        dfList: [dataframe, df],
+        axis: 0
+      });
+    }
+  }
+
+  return dataframe;
 }
